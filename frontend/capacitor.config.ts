13e11e87501f9d1bba0c54009217525660dcd0f6
jorogra <jr.graciano@outlook.com.br>
2026-01-29 20:03:15 -0300

    Versão Mobile Finalizada e Funcional

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.seu.app',
+  appName: 'StoreApp',
+  webDir: 'dist'
+};
+
+export default config;
